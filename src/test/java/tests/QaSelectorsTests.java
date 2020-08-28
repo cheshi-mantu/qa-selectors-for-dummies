29536ed7dbd2c15426f5122d08fc0b8425332ae0
cheshi-mantu <cheshi.mantu@gmail.com>
2020-08-28 14:25:10 +0300

    2 options for checking Submit button

--- src/test/java/tests/QaSelectorsTests.java
+++ src/test/java/tests/QaSelectorsTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.*;
 
 import static io.qameta.allure.Allure.step;
@@ -27,7 +28,7 @@ class QaSelectorsTests extends TestBase {
         });
     }
     @Test
-    @DisplayName("Open page click H1")
+    @DisplayName("Check if login and password fields are enabled when checkbox is checked")
     void pageOpenCheckBoxClick() {
         step ("Open html page", () -> open(htmlFilePath));
         step("Find checkbox by ID and click it", () -> {
@@ -37,6 +38,38 @@ class QaSelectorsTests extends TestBase {
         step("Check", () -> {
             $("#login").shouldBe(enabled);
             $("#secret").shouldBe(enabled);
+            $("[value='Submit']").shouldBe(enabled);
+        });
+    }
+    @Test
+    @DisplayName("Pseudo code based on Allure Report steps")
+    void pseudoOpenCheckBoxClickCheckFormFieldsEnabled() {
+        step ("Open html page");
+        step ("Find check box by its ID. Click ");
+        step ("Check if login field is enabled");
+        step ("Check if password field is enabled");
+        step ("Check if Submit button is enabled");
+    }
+    @Test
+    @DisplayName("Code based on Allure Report steps")
+    void pageOpenCheckBoxClickCheckFormFieldsEnabled() {
+        step ("Open html page", ()->{
+            open(htmlFilePath);
+        });
+        step ("Find check box by its ID. Click ", ()->{
+            $("#enabler").click();
+        });
+        step ("Check if login field is enabled", ()->{
+            $("#login").shouldBe(enabled);
+        });
+        step ("Check if password field is enabled", ()->{
+            $("#secret").shouldBe(enabled);
+        });
+//        step ("Check if Submit button is enabled", ()->{
+//            $("[value='Submit']").shouldBe(enabled);
+//        });
+        step ("Check if Submit button is enabled", ()->{
+            $$("input").findBy(attribute("value", "Submit")).shouldBe(enabled);
         });
     }
 }
